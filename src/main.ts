e3fe9094d97f975e100b8edd706e191fcf03f96b
Nobody-IArc <utk3970@naver.com>
2025-03-21 13:14:10 +0900

    유효성 검사 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'; // 유효성 검증을 위한 파이프 모듈
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // 전역 파이프 설정
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,14 +9,21 @@ import {
 } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
 
 @Controller('user') // 주소가 'user' 로 시작됨
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/create')
-  async createUser(@Body() user: User) {
-    return await this.userService.createUser(user);
+  async createUser(@Body() user: CreateUserDto) {
+    // DTO 에서 밭은 값과 필요한 값을 매핑해서 전송
+    const sendUser = new User();
+    sendUser.username = user.username;
+    sendUser.password = user.password;
+    sendUser.email = user.email;
+    sendUser.createdAt = new Date();
+    return await this.userService.createUser(sendUser);
   }
 
   @Get('/getUser/:email')
@@ -27,9 +34,15 @@ export class UserController {
   }
 
   @Put('/update/:email')
-  async updateUser(@Param('email') email: string, @Body() user: User) {
+  async updateUser(@Param('email') email: string, @Body() user: UpdateUserDto) {
     console.log(user);
-    return await this.userService.updateUser(email, user);
+    // DTO 에서 밭은 값과 기존 값(email)을 매핑해서 전송
+    const sendUser = new User();
+    sendUser.username = user.username;
+    sendUser.password = user.password;
+    sendUser.email = email;
+    console.log('Changed: \n', sendUser);
+    return await this.userService.updateUser(email, sendUser);
   }
 
   @Delete('/delete/:email')
